fe4b2f594d0bd06fb52c4615fe3f20c350bc78f2
Thru-Echoes <omuellerklein@berkeley.edu>
2019-05-29 15:06:17 -0700

    Established c2cmap.utils module with functional c2cmap.utils.ffmpeg_utils.py

--- utils/ffmpeg_utils.py
+++ utils/ffmpeg_utils.py
@@ -22,10 +22,21 @@ def get_video_to_frames_shell(input_path, start_time, end_time, output_path, fps
 ## Example use: sample 1 frame every 10 seconds from nest13B35A, on 01-August, video 00006.MTS 
 ## - for exactly 1 min between 1min 21.5sec to 2min 21.5sec 
 ## 
-## Oliver example 
-#shell_00006 = get_video_to_frames_shell("videodata/13B35A/0801_0841/00006.MTS", 00:01:21.500, 00:02:21.500, "imagedata/13B35A/0801_0841/00006", 1)
-#subprocess.run(shell_00006)
+## Oliver example
+"""
+> import utils 
+> import utils.ffmpeg_utils 
+> from utils import ffmpeg_utils
 
+shell_00006 = ffmpeg_utils.get_video_to_frames_shell(input_path = "videodata/13B35A/0801_0841/00006.MTS", 
+                                            start_time = "00:01:21.500", 
+                                            end_time = "00:02:21.500", 
+                                            output_path = "imagedata/13B35A/0801_0841/00006", 
+                                            fps = 1)
+subprocess.run(shell_00006)
+"""
+
+"""
 ## Mad Mike's example
 shell_command = get_video_to_frames_shell(input_path = "E:\20190327", 
                                           start_time = "00:00:00.000", 
@@ -33,3 +44,4 @@ shell_command = get_video_to_frames_shell(input_path = "E:\20190327",
                                           output_path = "E:\20190327\Frames",
                                           fps = 60)
 subprocess.run(shell_command)
+"""
